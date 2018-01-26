a452042e5e1cc0f6ecf0ff12f93e9bdae66bfa26
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-26 12:01:29 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content == 'Join them') {
+    if (message.author.id == 260241663018926080) {
     	message.channel.send('I better get in on this one');
         message.channel.send('!toke');
   	}
