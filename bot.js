e57da94be7aef9e85b1a9129a1a8fa9dbcdb0c48
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-26 12:11:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,22 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.author.id == 260241663018926080) {
+    if (message.author.id == 159985870458322944) {
     	message.channel.send('I better get in on this one');
         message.channel.send('!toke');
   	}
 });
 
+var swanreply = ["Sup Swan","Just staying lit fam","About to pack another bowl];
+
+client.on('message', message => {
+    if (message.author.id == 260241663018926080 && message.content === 'What\'s up smoke bot?') {
+    	for(i=0;i<3;i++){
+                 message.channel.send(swanreply[i]);
+                 }
+                 else {i=i-3}
+                 }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
