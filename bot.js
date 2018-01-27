cb827d4ccb2159b2db609ae691a11f112349770c
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-26 20:51:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ client.on('message', message => {
 });
 
 client.on('message', function() {
-    if (message.content === "$loop") { 
+    if (message.content.includes("loop")) { 
       var interval = setInterval (function () {
         message.channel.send("123")
       }, 1 * 1000); 
