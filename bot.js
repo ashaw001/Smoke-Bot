0413ad7f120acd7203ee02eaaa129c0b1eae2637
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-27 11:52:56 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,6 +44,7 @@ client.on('message', message => {
 
 client.on('message', message =>{
  if (message.author.id == 260241663018926080 && message.content.includes("!start_meme_mine_bot")) {
+	message.react("👌"); 
 for (i = 0; i < 10; i++) {
    setInterval(function() {
 	message.channel.send('!mine');
