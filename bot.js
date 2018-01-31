649a9d2e1df4ddf891f64e72206c379fabfa287e
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-31 12:05:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,13 @@ client.on('ready', () => {
 client.on('message', message =>{
  if (message.author.id == 260241663018926080 && message.content.includes("!start")) {
 	message.react("👍");  //Acknowledge
+function start(){
+var one = setTimeout(function(){step1(1)}, 1000);
+var two = setTimeout(function(){step2(1)}, 30000);
+var three = setTimeout(function(){step3(1)}, 35000);
+var four = setTimeout(function(){step4(1)}, 40000);
+var five = setTimeout(function(){step5(1)}, 45000);
+	}
 function step1(){
 	message.channel.send('!prestige YESIMSURE');
 	message.channel.send('!mine');
@@ -26,38 +33,7 @@ function step4(){
 function step5(){	
 	message.channel.send('!buy max miner');
 	}
-	
-var one = setInterval(function(){step1(1)}, 73000);
-var two = setInterval(function (){step2(1)}, 30000);
-var three = setInterval(function(){step3(1)}, 45000);
-var four = setInterval(function (){step4(1)}, 60000);
-var five = setInterval(function (){step5(1)}, 75000);
-}
-});
-
-
-
-client.on('message', message =>{
- if (message.author.id == 260241663018926080 && message.content.includes("!levelup")) {
-	message.react("👍");  //Acknowledge
-function stepone(){
-	message.channel.send('!buy max fracker');
-	}
-function steptwo(){	
-	message.channel.send('!buy max swarm');
-	}
-function stepthree(){
-	message.channel.send('!buy max robot');
-	}
-function stepfour(){	
-	message.channel.send('!buy max miner');
-	message.channel.send('!prestige YESIMSURE');
-	}
-	
-var one1 = setInterval(function(){stepone(1)}, 2000);
-var two2 = setInterval(function (){steptwo(1)}, 4000);
-var three3 = setInterval(function(){stepthree(1)}, 6000);
-var four4 = setInterval(function (){stepfour(1)}, 8000);
+var start = setInterval(function(){start(1)}, 71000);
 }
 });
 
