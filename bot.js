9e0ad9ceb46f6eda8d99ec09e102deadd7003ad3
ashaw001 <32914643+ashaw001@users.noreply.github.com>
2018-01-31 19:03:24 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,8 @@ client.on('ready', () => {
 client.on('message', message =>{
  if (message.author.id == 260241663018926080 && message.content.includes("!start")) {
 	message.react("👍");  //Acknowledge
-function runscript(){
+function strt () {
+function routine1(){
 	function step1(){
 	message.channel.send('!mine');
 	message.channel.send('!collect');
@@ -35,9 +36,89 @@ function runscript(){
 	var four = setTimeout(function() {step4(1)}, 50000);
 	var five = setTimeout(function() {step5(1)}, 60000);
 	}
-var strt = setInterval(function() {runscript(1)}, 70000);}
+function routine2(){
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	message.channel.send('!memes');
+	}
+	/*function step2(){	
+	message.channel.send('!buy max fracker');
+	}*/
+	function step3(){
+	message.channel.send('!buy max swarm');
+	}
+	function step4(){	
+	message.channel.send('!buy max robot');
+	}
+	function step5(){	
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
+	}
+	var one = setTimeout(function() {step1(1)}, 500);
+	//var two = setTimeout(function() {step2(1)}, 30000);
+	var three = setTimeout(function() {step3(1)}, 40000);
+	var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
+	}
+function routine3(){
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	message.channel.send('!memes');
+	}
+	/*function step2(){	
+	message.channel.send('!buy max fracker');
+	}
+	function step3(){
+	message.channel.send('!buy max swarm');
+	}*/
+	function step4(){	
+	message.channel.send('!buy max robot');
+	}
+	function step5(){	
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
+	}
+	var one = setTimeout(function() {step1(1)}, 500);
+	//var two = setTimeout(function() {step2(1)}, 30000);
+	//var three = setTimeout(function() {step3(1)}, 40000);
+	var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
+	}
+function routine4(){
+	function step1(){
+	message.channel.send('!mine');
+	message.channel.send('!collect');
+	message.channel.send('!memes');
+	}
+	/*function step2(){	
+	message.channel.send('!buy max fracker');
+	}
+	function step3(){
+	message.channel.send('!buy max swarm');
+	}
+	function step4(){	
+	message.channel.send('!buy max robot');
+	}*/
+	function step5(){	
+	message.channel.send('!buy max miner');
+	message.channel.send('!prestige YESIMSURE');
+	}
+	var one = setTimeout(function() {step1(1)}, 500);
+	//var two = setTimeout(function() {step2(1)}, 30000);
+	//var three = setTimeout(function() {step3(1)}, 40000);
+	//var four = setTimeout(function() {step4(1)}, 50000);
+	var five = setTimeout(function() {step5(1)}, 60000);
+	}
+var routineA = setTimeout(function() {routine1(1)}, 3000);
+var routineB = setTimeout(function() {routine2(1)}, 73000);
+var routineC = setTimeout(function() {routine3(1)}, 143000);
+var routineD = setTimeout(function() {routine4(1)}, 213000);
+		}
+	var initial = setInterval(function() {strt(1)}, 300000);
+ }
 });
-
 client.on('message', message =>{
  if (message.author.id == 260241663018926080 && message.content.includes("!levelup")) {
 	message.react("👍");  //Acknowledge
